5b32d2f3ab624a753a7507fb4a8d8dc0509d599c
Jessica Pring <jessicapring@outlook.com>
2021-06-09 19:52:28 +0100

    navbar component with props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar title='Nav' icon='fab fa-github' />
 
         <h1>Hello World</h1>
       </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'Navbar',
+    icon: 'fab fa-github'
+  }
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
+
   render() {
     return (
       <nav className="navbar">
         <h4>
-          <i className="fab fa-github"></i> Navbar
+          <i className={this.props.icon}></i> {this.props.title}
         </h4>
       </nav>
     )
