0708944639e324b4891806dbbff5da77ca5f93b1
Jessica Pring <jessicapring@outlook.com>
2021-06-09 19:47:31 +0100

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
+
         <h1>Hello World</h1>
-        <i className="fab fa-github"></i>
       </div>
     );
   }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <h4>
+          <i className="fab fa-github"></i> Navbar
+        </h4>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
