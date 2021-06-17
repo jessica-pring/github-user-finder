278ce46ab1fa823cc7ca8ceb851f6d8b9aa6ff8b
Jessica Pring <jessicapring@outlook.com>
2021-06-17 18:41:53 +0100

    reading data from api and generating user components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import './App.css';
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from './components/users/Users';
+import axios from 'axios';
+
+const url = "https://api.github.com/users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(url);
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar title='GitHub User Finder' icon='fab fa-github' />
 
         <div className="container">
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,36 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [
-        {
-          id: "id",
-          login: "mojombo",
-          avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-          html_url: "https://github.com/mojombo"
-        },
-        {
-          id: "id",
-          login: "defunkt",
-          avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-          html_url: "https://github.com/defunkt"
-        },      {
-          id: "id",
-          login: "pjhyett",
-          avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-          html_url: "https://github.com/pjhyett"
-        }
-      ]
-    }
-  }
- 
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <div>
             <UserItem key={user.id} user={user} />
           </div>
