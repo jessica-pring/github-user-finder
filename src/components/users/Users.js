d5048dfb9b56d8f7790bcbe24008b8e77a1d563a
Jessica Pring <jessicapring@outlook.com>
2021-06-17 19:01:19 +0100

    added loading spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <div>
             <UserItem key={user.id} user={user} />
           </div>
@@ -13,6 +17,11 @@ class Users extends Component {
       </div>
     )
   }
+}
+
+Users.PropTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
